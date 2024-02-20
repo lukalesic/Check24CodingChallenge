2df1ee1ef5f32dda07e479a2aeb2637aa4e3d9d1
lukalesic <lukalesic20@gmail.com>
2024-02-20 09:48:49 +0100

    Initial Commit

---
+++ Check24CodingChallenge/Check24CodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check24CodingChallengeApp.swift
+//  Check24CodingChallenge
+//
+//  Created by Luka Lešić on 20.02.2024..
+//
+
+import SwiftUI
+
+@main
+struct Check24CodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Check24CodingChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Check24CodingChallenge
+//
+//  Created by Luka Lešić on 20.02.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
